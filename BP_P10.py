f5968dfd91e043d348ba6bad5719a7dd12f2d094
go2collage <91822422+go2collage@users.noreply.github.com>
2023-03-11 22:35:54 +0530

    Add files via upload

---
+++ BP_P10.py
@@ -0,0 +1,16 @@
+# Basic Python Program
+
+# Largest number among three number
+
+num1 = 5
+num2 = 10
+num3 = 2
+
+if num1 > num2 and num1 > num3:
+    print(num1," is largest number.")
+
+elif num2 > num1 and num2 > num3:
+    print(num2," is largest number.")
+
+else:
+    print(num3," is largest number.")
